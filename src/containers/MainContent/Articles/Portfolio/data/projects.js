0b4f9f2b6829d523116c19d96d414a0d1797302a
Amandine Ameye <amandineameye@gmail.com>
2025-01-21 17:33:29 +0100

    New project pictures

--- src/containers/MainContent/Articles/Portfolio/data/projects.js
+++ src/containers/MainContent/Articles/Portfolio/data/projects.js
@@ -2,14 +2,14 @@ const projects = [
 	{
 		projectTitle: "Diabestie",
 		projectCategory: "React, Node.js, MongoDB, Redux",
-		imgLink: "/pictures/new_DB.png",
+		imgLink: "/pictures/DB.png",
 		gitHubLink: "https://github.com/amandineameye/Diabestie_React",
 		webLink: "https://diabestie-ecru.vercel.app/",
 	},
 	{
 		projectTitle: "Carbon Quest",
 		projectCategory: "React, Node.js, MongoDB, Unity, C#",
-		imgLink: "/pictures/new_CQ.png",
+		imgLink: "/pictures/CQ.png",
 		gitHubLink: "https://github.com/amandineameye/Carbon_Quest",
 		webLink: "https://super-carbon-quest.vercel.app/",
 	},
@@ -17,14 +17,14 @@ const projects = [
 	{
 		projectTitle: "Portfolio Website",
 		projectCategory: "React",
-		imgLink: "/pictures/new_PF.png",
+		imgLink: "/pictures/PF.png",
 		gitHubLink: "https://github.com/amandineameye/Portfolio",
 		webLink: "https://amandineameye.vercel.app/",
 	},
 	{
 		projectTitle: "Mastermind",
 		projectCategory: "Vanilla JS",
-		imgLink: "/pictures/new_MM.png",
+		imgLink: "/pictures/MM.png",
 		gitHubLink: "https://github.com/amandineameye/Exos_JS/tree/main/Mastermind",
 		webLink: "https://mastermindamandine.vercel.app/",
 	},
@@ -32,13 +32,13 @@ const projects = [
 	{
 		projectTitle: "Diabest Friend",
 		projectCategory: "Vanilla JS",
-		imgLink: "/pictures/new_DF.png",
+		imgLink: "/pictures/DF.png",
 		gitHubLink: "https://github.com/amandineameye/Diabest_Friend",
 	},
 	{
 		projectTitle: "Harry Potter Quiz",
 		projectCategory: "Vanilla JS",
-		imgLink: "/pictures/new_HP1.png",
+		imgLink: "/pictures/HP.png",
 		gitHubLink: "https://github.com/amandineameye/Quizz_Harry_Potter",
 	},
 ];
